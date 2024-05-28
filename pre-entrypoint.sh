c1239a43cd042c6a31c4a2fc44f85101e0cab11c
George Taylor <george.taylor@digital.justice.gov.uk>
2024-05-28 19:16:29 +0100

    renamed env var

--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -3,7 +3,7 @@
 set -xe
 
 echo "Pre-entrypoint script"
-aws s3 cp s3://${S3_BUCKET}/ /var/www/html/config/ --recursive
+aws s3 cp s3://${S3_BUCKET_CONFIG}/ /var/www/html/config/ --recursive
 
 # Execute the original entrypoint with its arguments
 exec /entrypoint.sh "$@"
