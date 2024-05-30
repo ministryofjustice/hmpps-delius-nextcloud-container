354e93dc42983cf1fd996e0af4a7daeaef7f62af
George Taylor <george.taylor@digital.justice.gov.uk>
2024-05-30 18:09:51 +0100

    add config load command

--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -3,7 +3,20 @@
 set -xe
 
 echo "Pre-entrypoint script"
-aws s3 cp s3://${S3_BUCKET_CONFIG}/ /var/www/html/config/ --recursive
+# cd /var/www/html
+# php occ
+
+# # su -s /bin/bash www-data -c "php /var/www/html/occ"
+if [ -z "$S3_BUCKET_CONFIG" ]; then
+    echo "S3_BUCKET_CONFIG is not set. will not copy config from S3."
+else
+    echo "S3_BUCKET_CONFIG is set to ${S3_BUCKET_CONFIG}"
+    aws s3 cp s3://${S3_BUCKET_CONFIG}/ /var/www/html/config/ --recursive
+fi
+
+# load config from s3 into nextcloud
+
+su -s /bin/bash www-data -c "php occ config:import /var/www/html/config/config.json"
 
 # Execute the original entrypoint with its arguments
 exec /entrypoint.sh "$@"
