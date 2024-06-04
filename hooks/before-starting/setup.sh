bb006813f48df29f0f719ade0a353b83465d9685
George Taylor <george.taylor@digital.justice.gov.uk>
2024-06-05 00:40:17 +0100

    fix setup script

---
+++ hooks/before-starting/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+
+# Check if Nextcloud is installed
+if php occ status | grep -q "installed: true"; then
+    echo "Nextcloud is already running startup scripts"
+
+    if [ -z "$S3_BUCKET_CONFIG" ]; then
+        echo "S3_BUCKET_CONFIG is not set. Will not copy config from S3."
+    else
+        echo "S3_BUCKET_CONFIG is set to ${S3_BUCKET_CONFIG}"
+        aws s3 cp "s3://${S3_BUCKET_CONFIG}/" /var/www/html/config/ --recursive
+    fi
+
+    # Set ownership of the config directory
+    chown -R www-data:www-data /var/www/html/config
+
+    # Import the Nextcloud config
+    cd /var/www/html/
+    php occ config:import ./config/config.json
+
+    # Check if LDAP_PASSWORD_SECRET_ARN is set
+    if [ -z "$LDAP_PASSWORD_SECRET_ARN" ]; then
+        echo "LDAP_PASSWORD_SECRET_ARN is not set. Aborting."
+    fi
+
+    LDAP_PASSWORD=$(aws secretsmanager get-secret-value --secret-id "${LDAP_PASSWORD_SECRET_ARN}" --query SecretString --output text)
+
+    # Set LDAP password
+    php occ ldap:set-config s01 ldapAgentPassword "${LDAP_PASSWORD}"
+else
+    echo "Nextcloud is not installed yet. Skipping startup scripts."
+    echo "pre/post installation scripts will run after installation instead."
+fi
